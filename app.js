6fde54a37a71bdaedb1bfe7a602b0f9f0b3549d2
rcypriano <rccypriano@gmail.com>
2023-08-08 21:04:49 -0300

    change: app/index.js to app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import { Accordion, Section, Alert, Button, Tab } from '../components/index.js'
+import { Accordion, Section, Alert, Button, Tab } from './components/index.js'
 
 //propriedades que vamos passar para a função Accordion
 const accordionProps = {
@@ -24,7 +24,7 @@ document.body.append(
                             Alert({ text: "Hello World yellow", color: "yellow"}),
                         ],
                     },
-                    
+
                     {
                         title: "Buttons",
                         children: [
@@ -36,34 +36,34 @@ document.body.append(
             }),
         ],
     }),
-);
-//     Section({
-//         title: Accordion.name,
-//         children: Accordion(accordionProps),
-//     }),
-//     Section({
-//         title: Alert.name,
-//         children: [
-//             Alert({text: "Hello World blue", color: "blue"}),
-//             Alert({text: "Hello World green", color: "green"}),
-//             Alert({text: "Hello World red", color: "red"}),
-//             Alert({text: "Hello World yellow", color: "yellow"}),
-//         ],
-//     }),
-//     Section({
-//         title: Button.name,
-//         children: [
-//             Button({children: "Botão 1"}),
-//             Button({children: "Botão 2", type: "button"}),
-//             Button({children: 'Botão 3', type: "button", color: "red" }),
-//             Button({
-//                 children: 'Botão 4',
-//                 type: "button",
-//                 color: "green",
-//                 onclick: function () {
-//                     alert("on click funcionando");
-//                 }
-//             }),
-//         ]
-//     }),
-// );
+
+    Section({
+        title: Accordion.name,
+        children: Accordion(accordionProps),
+    }),
+    Section({
+        title: Alert.name,
+        children: [
+            Alert({text: "Hello World blue", color: "blue"}),
+            Alert({text: "Hello World green", color: "green"}),
+            Alert({text: "Hello World red", color: "red"}),
+            Alert({text: "Hello World yellow", color: "yellow"}),
+        ],
+    }),
+    Section({
+        title: Button.name,
+        children: [
+            Button({children: "Botão 1"}),
+            Button({children: "Botão 2", type: "button"}),
+            Button({children: 'Botão 3', type: "button", color: "red" }),
+            Button({
+                children: 'Botão 4',
+                type: "button",
+                color: "green",
+                onclick: function () {
+                    alert("on click funcionando");
+                }
+            }),
+        ]
+    }),
+)
